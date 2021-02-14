685fb10294af0125a74ea111625c9b49099e22a4
josemapt <josema@github.com>
2021-02-14 17:57:57 +0100

    minor changes

--- 4-theme.sh
+++ 4-theme.sh
@@ -8,16 +8,18 @@
 # theme ---------------------------------------------------------
 echo "==> Starting theme configuration..."
 
-[[ ! -d ~/.config/gtk-3.0/ ]] && mkdir -p ~/.config/gtk-3.0
-[[ ! -f ~/.config/gtk-3.0/settings.ini ]] && echo "[Settings]" > ~/.config/gtk-3.0/settings.ini
-
 echo "changing cursor theme"
 tar -xf asetts/165371-Breeze.tar.gz
 sudo mv Breeze/ /usr/share/icons/
-echo "gtk-cursor-theme-name = Breeze" >> ~/.config/gtk-3.0/settings.ini
+gsettings set org.gnome.desktop.interface cursor-theme 'Breeze'
 sudo sed -i 's/Adwaita/Breeze/' /usr/share/icons/default/index.theme
 
 echo "changing icon theme"
 tar -xf asetts/papirus-icon-theme.tar.zst
 sudo mv usr/share/icons/Pa* /usr/share/icons/
-echo "gtk-cursor-theme-name = Breeze" >> ~/.config/gtk-3.0/settings.ini
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+
+echo "changing gtk theme"
+tar -xf assets/Matcha-sea.tar.gz
+sudo mv usr/share/themes/Matcha-* /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme 'Matcha-sea'
