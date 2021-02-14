ae75f5bf28c1f5572463b66d134da495418e2ad2
josemapt <josema@github.com>
2021-02-14 15:35:37 +0100

    minor changes

--- preinstall.sh
+++ preinstall.sh
@@ -12,27 +12,29 @@ fi
 
 echo "==> Refreshing mirrorlist (spain)..."
 #timedatectl set-ntp true
-curl -o mirrorlist_spain https://archlinux.org/mirrorlist/?country=ES&protocol=http&protocol=https&ip_version=4
-curl -o mirrorlist_germany https://archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4
-curl -o mirrorlist_france https://archlinux.org/mirrorlist/?country=FR&protocol=http&protocol=https&ip_version=4
+curl -s -o mirrorlist_spain https://archlinux.org/mirrorlist/?country=ES&protocol=http&protocol=https&ip_version=4
+curl -s -o mirrorlist_germany https://archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4
+curl -s -o mirrorlist_france https://archlinux.org/mirrorlist/?country=FR&protocol=http&protocol=https&ip_version=4
 
 cat mirrorlist_spain mirrorlist_france mirrorlist_germany > /etc/pacman.d/mirrorlist
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+pacman -Sy
 
 # disk (mbr) ------------------------------------------------------------------------
-echo "==> Preparing disk for installation (mbr)..."
+echo "==> Preparing disk for insmtallation (mbr)..."
 
 echo -n "Enter disk name (/dev/sda, /dev/nvme0n1 ...): "
 read DISK
 
 echo "formating ${DISK}1 as linux swap"
-mkswap "${DISK}1"
-swapon "${DISK}1"
+mkswap ${DISK}1
+swapon ${DISK}1
 
 echo "formating ${DISK}2 as ext4"
-mkfs.ext4 "${DISK}2"
+mkfs.ext4 ${DISK}2
 
 echo "mounting ${DISK}2 at /mnt"
-mount "/dev/${DISK}2" /mnt
+mount /dev/${DISK}2 /mnt
 
 
 # Arch Linux installation -------------------------------------------------------------
