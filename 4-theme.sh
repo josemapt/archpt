526e67c2376b1abe109f67ec5b1acc8c0043aff6
josemapt <josema@github.com>
2021-02-14 17:41:03 +0100

    minor changes

--- 4-theme.sh
+++ 4-theme.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Some gnome settings -------------------------------------------
+
+
+
+
+# theme ---------------------------------------------------------
+thFile=".config/gtk-3.0/settings.ini"
+
 echo "==> Starting theme configuration..."
 
-tar -xf asetts/*
+[[ ! -d .config/gtk-3.0/ ]] && mkdir -p .config/gtk-3.0
+[[ ! -f $thFile ]] && echo "[Settings]" > $thFile
+
+echo "changing cursor theme"
+tar -xf asetts/165371-Breeze.tar.gz
+sudo mv Breeze/ /usr/share/icons/
+echo "gtk-cursor-theme-name = Breeze" >> $thFile
+
+echo "changing icon theme"
+tar -xf asetts/papirus-icon-theme.tar.zst
+sudo mv Breeze/ /usr/share/icons/
+echo "gtk-cursor-theme-name = Breeze" >> $thFile
