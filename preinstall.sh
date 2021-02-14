d0ae81177e9d8a82bff6e469033ef5532273b56b
josemapt <josema@github.com>
2021-02-14 12:03:37 +0100

    initial setup

---
+++ preinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "==> Checking network connection..."
+if ping -c 1 google.com >/dev/null
+then
+    echo "done"
+else
+    echo "Network not found"
+    echo "Please connect to Internet before running this script"
+    exit -1
+fi
+
+echo "==> Mounting /dev/nvme0n1p3 at /mnt..."
+mount /dev/nvme0n1p3 /mnt
+echo "done"
+
+echo "==> Installing base pakages..."
+pacstrap /mnt base base-devel linux linux-firmware
+echo "done"
+
+echo "==> Generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "done"
+
+echo "==> Arch-chroot at /mnt"
+arch-chroot /mnt
