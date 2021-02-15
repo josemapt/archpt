165432c8d8c0065b7b37d5367a3a42d8ce1af98a
josemapt <josema@github.com>
2021-02-15 15:25:42 +0100

    minor changes

--- 4-theme.sh
+++ 4-theme.sh
@@ -63,11 +63,12 @@ sudo bash -c "echo 'NoDisplay=true'  >> /usr/share/applications/qvidcap.desktop"
 sudo bash -c "echo 'NoDisplay=true'  >> /usr/share/applications/lstopo.desktop"
 sudo bash -c "echo 'NoDisplay=true'  >> /usr/share/applications/nm-connection-editor.desktop"
 
-echo "changing terminal default gtk-theme"
+echo "changing terminal theme"
 gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ font 'SauceCodePro Nerd Font 22'
 
 echo "changing default shell"
 sudo chsh -s /bin/fish josema # need change
-
+/bin/fish -c "set -U fish_greeting"
 
 echo "Installation finished"
