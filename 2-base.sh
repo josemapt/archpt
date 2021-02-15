3055dd7be8155f2795d71f8cd51d5ef98338b62e
josemapt <josema@github.com>
2021-02-15 17:10:52 +0100

    minor changes

--- 2-base.sh
+++ 2-base.sh
@@ -1,38 +1,4 @@
 
 echo "==> Installing Base System..."
 
-PKGS=(
-
-    # --- drivers and utils
-        'xf86-video-intel'
-        'nvidia'
-        'neovim'
-        'unzip'
-        'fish'
-        'fd'
-        'exa'
-        'ripgrep'
-        'sd'
-
-    # --- desktop enviroment
-        'gnome-shell'
-        'gnome-terminal'
-        'gnome-control-center'
-        'gnome-tweaks'
-        #'gdm'
-
-    # --- programs
-        'nautilus'
-        'evince'
-        'totem'
-        'eog'
-        'gnome-system-monitor'
-        'file-roller'
-        'baobab'
-        'gnome-screenshot'
-)
-
-for PKG in "${PKGS[@]}"; do
-    echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --noconfirm --needed
-done
+sudo pacman -S --noconfirm --needed <pakages.txt
--- 3-aur.sh
+++ 3-aur.sh
@@ -10,26 +10,7 @@ sudo sed -i '/^#NewsOnUpgrade/a BuildDir = \/tmp' /etc/paru.conf
 
 echo "==> Installing aur pakages..."
 
-PKGS=(
-
-    # --- programs
-        'brave-bin'
-        'vscodium-bin'
-    
-    # --- themes and fonts
-        'yaru-sound-theme'
-        'nerd-fonts-source-code-pro'
-    
-    # --- plymouth
-        'plymouth'
-        #'gdm-plymouth'
-)
-
-for PKG in "${PKGS[@]}"; do
-    echo "INSTALLING: ${PKG}"
-    paru -S --noconfirm "$PKG"
-done
+paru -S --noconfirm <pakages_aur.txt
 
 paru -S gdm-plymouth
-
 sudo systemctl enable gdm
